e42e96d6c21936e3b1fa655a90a2f878733de285
Federico Melis <70884674+fedemelis@users.noreply.github.com>
2024-10-30 21:34:45 +0100

    non va

---
+++ IoT_LoRa_bridge.ino
@@ -0,0 +1,58 @@
+union{
+  float f;
+  byte b[4];
+} _f;
+
+struct Slot{
+  uint8_t zone_; // parking zone
+  uint8_t id_; // parking id
+  float lat_; // parking latitude
+  float lon_; // parking longitude
+
+  /**
+  * @brief Constructor for a slot.
+  *
+  * @param zone The zone that the slot belongs to.
+  * @param id The unique id wrt zone.
+  * @param lat Latitude of parking slot.
+  * @param lon Longitude of parking slot.
+  * 
+  *
+  */
+  Slot(uint8_t zone, uint8_t id, double lat, double lon) : 
+    zone_(zone), id_(id), lat_(lat), lon_(lon) {}
+
+  // empty constructor
+  Slot() : zone_(0), id_(0), lat_(0), lon_(0) {}
+};
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  if(Serial.available() > 0){
+    delay(2000);
+    if(byte(Serial.read()) == 0xFF){
+      int len = Serial.read();
+      if(Serial.available() < len){
+        while(Serial.read() != 0xFE){};
+      }
+      int zone = Serial.read();
+      int id = Serial.read();
+      //Serial.write(zone);
+      //Serial.write(id);
+      float lat = Serial.parseFloat();
+      _f.f = lat;
+      for (int i = 0; i < 4; i++) {
+        Serial.write(_f.b[i]); // Invia i 4 byte del float
+      }
+      float lon = Serial.parseFloat();
+      _f.f = lon;
+      for (int i = 0; i < 4; i++) {
+        Serial.write(_f.b[i]); // Invia i 4 byte del float
+      }
+    }
+  }
+}
